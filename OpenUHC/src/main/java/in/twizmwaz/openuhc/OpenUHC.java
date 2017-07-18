aa0098c0ed371c2dbf41487d0192956a9a1c76c1
Kevin Phoenix <twizmwazin@gmail.com>
2017-07-17 20:56:43 -0400

    Initial commit

---
+++ OpenUHC/src/main/java/in/twizmwaz/openuhc/OpenUHC.java
@@ -0,0 +1,12 @@
+package in.twizmwaz.openuhc;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OpenUHC extends JavaPlugin {
+
+  public void onEnable() {
+    Bukkit.getLogger().info("OpenUHC has started.");
+  }
+
+}
